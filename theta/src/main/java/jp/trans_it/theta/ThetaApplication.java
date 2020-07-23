e0caa926fc61a72e759cb598367d4c6617127885
Satoshi Tanaka <satstnka@gmail.com>
2020-07-23 09:45:57 +0900

    create project.

---
+++ theta/src/main/java/jp/trans_it/theta/ThetaApplication.java
@@ -0,0 +1,13 @@
+package jp.trans_it.theta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThetaApplication.class, args);
+	}
+
+}
---
+++ theta/src/test/java/jp/trans_it/theta/ThetaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.trans_it.theta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
